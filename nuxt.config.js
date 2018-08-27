2b65c54478c786aa160fbd7a6a25cfa25836ff7a
DaveP <dave@wearecrystal.uk>
2018-08-27 18:06:06 +0100

    Meta

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,12 +3,40 @@ module.exports = {
     title: 'hundo',
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       {
         hid: 'description',
         name: 'description',
         content: 'Information about the Hundo Discord bot',
       },
+      {
+        hid: 'title',
+        name: 'title',
+        content: 'Rosebella | Neckwear, Wristwear & Earrings',
+      },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { name: 'msapplication-TileColor', content: '#bb1a34' },
+      { name: 'theme-color', content: '#ffffff' },
+    ],
+    link: [
+      {
+        rel: 'apple-touch-icon',
+        sizes: '180x180',
+        href: '/apple-touch-icon.png',
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '32x32',
+        href: '/favicon-32x32.png',
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '16x16',
+        href: '/favicon-16x16.png',
+      },
+      { rel: 'manifest', href: '/site.webmanifest' },
+      { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#bb1a34' },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
