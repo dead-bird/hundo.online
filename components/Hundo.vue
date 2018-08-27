30c5639131fd359f1244c087beedc0037bc5ed38
DaveP <dave@wearecrystal.uk>
2018-08-27 15:08:52 +0100

    Template

---
+++ components/Hundo.vue
@@ -0,0 +1,43 @@
+<script>
+export default {
+  props: ['text'],
+
+  data: () => ({
+    width: 100,
+    height: 100,
+  }),
+};
+</script>
+
+<template>
+<!-- <?xml version="1.0"?> -->
+    <svg xmlns="http://www.w3.org/2000/svg" :width="width" :height="height" :viewBox="`0 0 ${width} 1`">
+    <title>Hundo</title>
+
+    <g class="hundo">
+      <text id="text" x="50%" text-anchor="middle">{{ text }}</text>
+      
+      <g :transform="`translate(${width / 2 - 23.8},5)`">
+        <path id="path34" d="M11.67,15.78a2.5,2.5,0,0,1,0-4.92A103.88,103.88,0,0,1,37.9,10.6a2.51,2.51,0,0,1-.49,5,101,101,0,0,0-24.81.2,2.66,2.66,0,0,1-.89,0" fill="#bb1a34"/>
+        <path id="path38" d="M2,6.3a2.5,2.5,0,0,1,0-4.91c.71-.13,17.8-3.13,41.48.11a2.5,2.5,0,0,1-.68,5C20,3.33,3.11,6.28,2.94,6.3A2.32,2.32,0,0,1,2,6.3" fill="#bb1a34"/>
+      </g>
+    </g>
+  </svg>
+</template>
+
+
+<style>
+.hundo {
+  transform: rotate(-10deg);
+  transform-origin: bottom;
+}
+
+#text {
+  font-size: 28px;
+  font-family: sans-serif;
+  font-weight: 800;
+  fill: #bb1a34;
+  font-style: italic;
+}
+</style>
+
